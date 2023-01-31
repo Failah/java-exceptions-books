332ce6c2fccebedf1dae8e3bce8e51ac944ec138
ValerioScharizza <valerio.schiarizza@live.it>
2023-01-31 14:14:26 +0100

    done exercise

--- src/org/lessons/java/exceptionsbooks/Book.java
+++ src/org/lessons/java/exceptionsbooks/Book.java
@@ -1,5 +1,75 @@
+/*
+ * Vogliamo gestire il catalogo di libri di una biblioteca.
+Un libro è composto da :
+- titolo
+- numero pagine
+- autore
+- editore
+Scrivere un programma che inserisce n libri in un array.
+Chiede i dati di un libro (titolo, numero pagine, autore, editore), istanzia un nuovo oggetto della classe Book e lo inserisce nell’array.
+Lo fa per il numero di elementi previsti nell’array.
+Quando vengono inseriti i dati, verificare che siano corretti (es. non accettare titolo o autore o editore vuoto, numero pagine ≤ 0). 
+Se ci sono errori, lanciare un’eccezione e gestirla mostrando a video il tipo di errore.
+Implementare i getter e setter di tutti gli attributi del libro gestendo anche in questo caso eventuali errori di dati non corretti 
+(che generano quindi eccezione).
+ */
+
 package org.lessons.java.exceptionsbooks;
 
-public class Book {
+class Book {
+	private String title;
+	private int pages;
+	private String author;
+	private String publisher;
 
+	public Book(String title, int pages, String author, String publisher) {
+		this.title = title;
+		this.pages = pages;
+		this.author = author;
+		this.publisher = publisher;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) throws Exception {
+		if (title == null || title.isEmpty()) {
+			throw new Exception("Title cannot be empty");
+		}
+		this.title = title;
+	}
+
+	public int getPages() {
+		return pages;
+	}
+
+	public void setPages(int pages) throws Exception {
+		if (pages <= 0) {
+			throw new Exception("Number of pages must be positive");
+		}
+		this.pages = pages;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) throws Exception {
+		if (author == null || author.isEmpty()) {
+			throw new Exception("Author cannot be empty");
+		}
+		this.author = author;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) throws Exception {
+		if (publisher == null || publisher.isEmpty()) {
+			throw new Exception("Publisher cannot be empty");
+		}
+		this.publisher = publisher;
+	}
 }
--- src/org/lessons/java/exceptionsbooks/Main.java
+++ src/org/lessons/java/exceptionsbooks/Main.java
@@ -1,5 +1,74 @@
+/*
+ * Vogliamo gestire il catalogo di libri di una biblioteca.
+Un libro è composto da :
+- titolo
+- numero pagine
+- autore
+- editore
+Scrivere un programma che inserisce n libri in un array.
+Chiede i dati di un libro (titolo, numero pagine, autore, editore), istanzia un nuovo oggetto della classe Book e lo inserisce nell’array.
+Lo fa per il numero di elementi previsti nell’array.
+Quando vengono inseriti i dati, verificare che siano corretti (es. non accettare titolo o autore o editore vuoto, numero pagine ≤ 0). 
+Se ci sono errori, lanciare un’eccezione e gestirla mostrando a video il tipo di errore.
+Implementare i getter e setter di tutti gli attributi del libro gestendo anche in questo caso eventuali errori di dati non corretti 
+(che generano quindi eccezione).
+ */
+
 package org.lessons.java.exceptionsbooks;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	public static void main(String[] args) throws Exception {
+		Scanner s = new Scanner(System.in);
+		System.out.print("Enter number of books: ");
+		int n = s.nextInt();
+		s.nextLine();
+		Book[] books = new Book[n];
+
+		for (int i = 0; i < n; i++) {
+			System.out.println("Enter book #" + (i + 1) + " details:");
+			System.out.println("Title: ");
+			String title = s.nextLine();
+			System.out.println("Pages: ");
+			int pages = s.nextInt();
+			s.nextLine();
+			System.out.println("Author: ");
+			String author = s.nextLine();
+			System.out.println("Publisher: ");
+			String publisher = s.nextLine();
+
+			try {
+				if (title.isEmpty()) {
+					throw new Exception("Title cannot be empty!");
+				}
+				if (pages <= 0) {
+					throw new Exception("Pages cannot be 0 or negative!");
+				}
+				if (author.isEmpty()) {
+					throw new Exception("Author cannot be empty!");
+				}
+				if (publisher.isEmpty()) {
+					throw new Exception("Publisher cannot be empty!");
+				}
+
+				books[i] = new Book(title, pages, author, publisher);
+			} catch (Exception e) {
+				System.out.println("There is an Error in your input: " + e.getMessage());
+				i--;
+			}
+		}
+
+		System.out.println("\nHere is your books catalog:");
+		for (int i = 0; i < n; i++) {
+			System.out.println("Book #" + (i + 1) + ":");
+			System.out.println("Title: " + books[i].getTitle());
+			System.out.println("Pages: " + books[i].getPages());
+			System.out.println("Author: " + books[i].getAuthor());
+			System.out.println("Publisher: " + books[i].getPublisher());
+			System.out.println(" ");
+		}
+
+		s.close();
+	}
 }
